f3064268cbeedf8688b5aa76422e61f0633a4ea8
GITUSER <hudson@inboundnow.com>
2017-01-07 16:24:18 -0600

    changing if use strategy for cleaner nesting

--- classes/class.assigned-agents-form-submissions.php
+++ classes/class.assigned-agents-form-submissions.php
@@ -25,107 +25,106 @@ if(!class_exists('Inbound_Assigned_Agents_Inbound_Forms_Submissions')){
 			$form_values = get_post_meta($lead['form_id'], 'inbound_form_values');
 			parse_str($form_values[0], $form_values);
 			
-			if($form_values['inbound_shortcode_inbound_assign_agent_enable'] == 'on'){
+			if(!isset($form_values['inbound_shortcode_inbound_assign_agent_enable']) || $form_values['inbound_shortcode_inbound_assign_agent_enable'] != 'on') {
+				return;
+			}
 			
-				/*if rotating lead assignment has been selected in the extension settings*/
-				if($settings == 1){
-					
-					/*get the rotating assignment counter from the form meta*/
-					$rotation_counter = get_post_meta($lead['form_id'], 'inbound_assign_agents_rotation_counter', true);
-					
-					/*get the number of agents assigned to the form*/
-					$agent_count = count($form_values['inbound_shortcode_inbound_assign_agent']);
-					
-					/*get the agent to assign to with the remainder of dividing the counter by the agent count*/
-					$pointer = $rotation_counter % $agent_count;
-					
-					/*get the agent id*/
-					$agent_id = $form_values['inbound_shortcode_inbound_assign_agent'][$pointer];
+			/*if rotating lead assignment has been selected in the extension settings*/
+			if($settings == 1){
+
+				/*get the rotating assignment counter from the form meta*/
+				$rotation_counter = get_post_meta($lead['form_id'], 'inbound_assign_agents_rotation_counter', true);
+
+				/*get the number of agents assigned to the form*/
+				$agent_count = count($form_values['inbound_shortcode_inbound_assign_agent']);
+
+				/*get the agent to assign to with the remainder of dividing the counter by the agent count*/
+				$pointer = $rotation_counter % $agent_count;
+
+				/*get the agent id*/
+				$agent_id = $form_values['inbound_shortcode_inbound_assign_agent'][$pointer];
+
+				/*assign to lead groups if set to*/
+				if($form_values['inbound_shortcode_inbound_assign_to_agent_lead_group_enable'] == 'on'){
+
+					/*shorten the group variable*/
+					$groups = $form_values['inbound_shortcode_inbound_assign_to_agent_lead_group'];
+
+
+					for($i = 0; $i < $agent_count; $i++){
+						/*check_if_under_limits returns false if the group is not under the limit*/
+						$checked_groups = Inbound_Assigned_Agents_Management::check_if_under_limits($agent_id, $groups);
+						if($checked_groups !== false){
+							break;
+						}else{
+							//move the pointer one to try again
+							$pointer++;
+							$agent_id = $form_values['inbound_shortcode_inbound_assign_agent'][$pointer];
+						}
+					}
+						/*if there is an agent who's leads are under the limit*/
+						if($checked_groups !== false){
+							$data = array(
+										'execution' => 'add-leads',
+										'agent_ids' => array( $agent_id ),
+										'lead_groups' => $checked_groups,
+										'lead_ids' => array($lead['id']),
+							);
+
+							/*assign the lead to the agent and put the lead in the group/s*/
+							Inbound_Assigned_Agents_Management::edit_term_lead_group_data($data);
+
+						}
+
+					}else{
+						/*if the user opted not to put leads into groups, just add the lead to the agent*/
+							$data = array(
+										'execution' => 'add-leads',
+										'agent_ids' => array( $agent_id ),
+										'lead_ids' => array($lead['id']),
+							);
+
+							Inbound_Assigned_Agents_Management::edit_term_lead_group_data($data);
+					}
+
+				/*and update the counter for next time*/
+				$update_meta = update_post_meta($lead['form_id'], 'inbound_assign_agents_rotation_counter', (int)$rotation_counter + 1);
+
+			}else{
+				/**if leads are to be assigned to all form agents:**/
+
+				/*loop through the assigned agents*/
+				foreach($form_values['inbound_shortcode_inbound_assign_agent'] as $agent_id){
 
 					/*assign to lead groups if set to*/
 					if($form_values['inbound_shortcode_inbound_assign_to_agent_lead_group_enable'] == 'on'){
-						
 						/*shorten the group variable*/
 						$groups = $form_values['inbound_shortcode_inbound_assign_to_agent_lead_group'];
-					
-	
-						for($i = 0; $i < $agent_count; $i++){
-							/*check_if_under_limits returns false if the group is not under the limit*/
-							$checked_groups = Inbound_Assigned_Agents_Management::check_if_under_limits($agent_id, $groups);
-							if($checked_groups !== false){
-								break;
-							}else{
-								//move the pointer one to try again
-								$pointer++;
-								$agent_id = $form_values['inbound_shortcode_inbound_assign_agent'][$pointer];
-							}
-						}
-							/*if there is an agent who's leads are under the limit*/
-							if($checked_groups !== false){			
-								$data = array(
-											'execution' => 'add-leads',
-											'agent_ids' => array( $agent_id ),
-											'lead_groups' => $checked_groups,
-											'lead_ids' => array($lead['id']),
-								);
-								
-								/*assign the lead to the agent and put the lead in the group/s*/	
-								Inbound_Assigned_Agents_Management::edit_term_lead_group_data($data);
 
-							}
-						
-						}else{
-							/*if the user opted not to put leads into groups, just add the lead to the agent*/
-								$data = array(
-											'execution' => 'add-leads',
-											'agent_ids' => array( $agent_id ),
-											'lead_ids' => array($lead['id']),
-								);
-											
-								Inbound_Assigned_Agents_Management::edit_term_lead_group_data($data);
-						}
+						$checked_groups = Inbound_Assigned_Agents_Management::check_if_under_limits($agent_id, $groups);
 
-					/*and update the counter for next time*/
-					$update_meta = update_post_meta($lead['form_id'], 'inbound_assign_agents_rotation_counter', (int)$rotation_counter + 1);
-					
-				}else{
-					/**if leads are to be assigned to all form agents:**/
-				
-					/*loop through the assigned agents*/
-					foreach($form_values['inbound_shortcode_inbound_assign_agent'] as $agent_id){
-						
-						/*assign to lead groups if set to*/
-						if($form_values['inbound_shortcode_inbound_assign_to_agent_lead_group_enable'] == 'on'){
-							/*shorten the group variable*/
-							$groups = $form_values['inbound_shortcode_inbound_assign_to_agent_lead_group'];
-							
-							$checked_groups = Inbound_Assigned_Agents_Management::check_if_under_limits($agent_id, $groups);
-							
-							if($checked_groups !== false){
-								$data = array(
-											'execution' => 'add-leads',
-											'agent_ids' => array( $agent_id ),
-											'lead_groups' => $checked_groups,
-											'lead_ids' => array($lead['id']),
-								);
-							
-								Inbound_Assigned_Agents_Management::edit_term_lead_group_data($data);
-							}
-						}else{
+						if($checked_groups !== false){
+							$data = array(
+										'execution' => 'add-leads',
+										'agent_ids' => array( $agent_id ),
+										'lead_groups' => $checked_groups,
+										'lead_ids' => array($lead['id']),
+							);
+
+							Inbound_Assigned_Agents_Management::edit_term_lead_group_data($data);
+						}
+					}else{
 						/**if not set to put in groups, just assign the leads to agents**/
 								$data = array(
 											'execution' => 'add-leads',
 											'agent_ids' => array( $agent_id ),
 											'lead_ids' => array($lead['id']),
 								);
-											
+
 						/*add lead to the agent term for default taxonomy functionality*/
 						Inbound_Assigned_Agents_Management::edit_term_lead_group_data($data);
-						}
 					}
-				
 				}
-			
 			}
 
 		}
